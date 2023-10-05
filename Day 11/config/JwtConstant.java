282b928862709669bfb1dbf5885ade66735ac9f7
santhosh171203 <116015183+santhosh171203@users.noreply.github.com>
2023-10-05 10:46:17 +0530

    Add files via upload

---
+++ Day 11/config/JwtConstant.java
@@ -0,0 +1,9 @@
+package com.tech3.srinithi.config;
+
+
+public class JwtConstant {
+	
+	public static final String SECRET_KEY="kjhgtyedyesrdtfyhuiytiyuiyitu5ryguky7i6r5ydfhkgytrysdfh";
+	public static final String JWT_HEADER="Authorization";
+
+}
