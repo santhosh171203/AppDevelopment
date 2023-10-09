62bbb8c1ab18b2a64ee1fc7e9d858035c07357bd
santhosh171203 <116015183+santhosh171203@users.noreply.github.com>
2023-10-09 12:05:14 +0530

    Add files via upload

---
+++ Day 15/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.tech3.srinithi.controller;
+
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tech3.srinithi.exception.UserException;
+import com.tech3.srinithi.model.User;
+import com.tech3.srinithi.service.UserService;
+
+
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+	
+	private UserService userService;
+	
+	public UserController(UserService userService) {
+		this.userService=userService;
+	}
+	
+	@GetMapping("/profile")
+	public ResponseEntity<User> getUserProfileHandler(@RequestHeader("Authorization") String jwt) throws UserException{
+
+		System.out.println("/api/users/profile");
+		User user=userService.findUserProfileByJwt(jwt);
+		return new ResponseEntity<User>(user,HttpStatus.ACCEPTED);
+	}
+
+}
